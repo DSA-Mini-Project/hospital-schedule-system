510e92c590b33f33ee9a483613dcab02d8aac00d
Akash Bagchi <72804789+akashbagchi@users.noreply.github.com>
2020-11-15 12:08:34 +0530

    Update maincode.c
    added automatic rescheduling

--- maincode.c
+++ maincode.c
@@ -1,10 +1,17 @@
 //Program to implement automatic dynamic schedule system for a hospital with multiple working staff
 //Contributors : Akash Bagchi   Akshaya Nadathur   Anirudh Preeth   Anup Venkat
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<malloc.h>
 #include<string.h>
+
+
+//Arrays to represent the occupied or vacant status of the timeslots in staff members' schedules (0 represents occupied)
+char physician_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
+char gensurgeon_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
+char radiologist_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
+
+
 
 typedef struct node
 {
@@ -89,12 +96,196 @@ void printconfirmation(node * head, int timeslot, char name[], char type[], char
     printf("\n\nHi %s. Your %s appointment for %s has been successfully booked!\n",name, type, time);
 }
 
+
+void reschedule(node * head, int atype, int startpos)
+{
+    char dicttime[24][8]={"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
+    int elen, alen, oldstart;
+    char oldtype[25], con[10];
+    if(atype==1 || atype==2 || atype==7)
+        elen = 1;
+    else if(atype==3 || atype==4 || atype==5 || atype==8)
+        elen = 2;
+    else
+        elen = 6;
+
+    node * old = head;
+    while(old->next!=NULL)
+    {
+        if(old->timeslot==startpos)
+            break;
+        old = old->next;
+    }
+
+    node * cur = old;
+    for(int i=0; i<elen; i++)
+        cur = cur->next;
+
+    while(cur->next != NULL)
+    {
+        if(!strcmp(cur->reschedule_preference, "y"))
+        {
+            if(!strcmp(cur->type,"General Consultancy") || !strcmp(cur->type,"Blood Test/Vaccinations") || !strcmp(cur->type,"X-Ray"))
+                alen = 1;
+            else if(!strcmp(cur->type,"ECG Test") || !strcmp(cur->type, "Routine Checkup") || !strcmp(cur->type, "Minor Surgery") || !strcmp(cur->type, "Ultrasound"))
+                alen = 2;
+            else
+                alen = 6;
+
+            if(alen<=elen)
+            {
+                oldstart = cur->timeslot;
+                strcpy(oldtype, cur->type);
+                strcpy(con, cur->contact_number);
+
+                for(int i=0; i<alen; i++)
+                {
+                    insert_new_appointment(head, startpos+i, cur->name, cur->contact_number, cur->type, cur->reschedule_preference);
+                }
+
+                for(int i=0; i<alen; i++)
+                {
+                    strcpy(cur->name, "TBE");
+                    strcpy(cur->contact_number, "TBE");
+                    strcpy(cur->type, "TBE");
+                    strcpy(cur->reschedule_preference, "TBE");
+
+                    if(!strcmp(oldtype,"General Consultancy") || !strcmp(oldtype,"Blood Test/Vaccinations") || strcmp(oldtype,"X-Ray"))
+                        strcmp(physician_timeslot_availability[cur->timeslot-1],dicttime[oldstart-1+i]);
+                    else if(!strcmp(cur->type,"ECG Test") || !strcmp(cur->type, "Routine Checkup") || !strcmp(cur->type, "Minor Surgery") || !strcmp(cur->type, "Ultrasound"))
+                        strcmp(gensurgeon_timeslot_availability[cur->timeslot-1],dicttime[oldstart-1+i]);
+                    else
+                        strcmp(radiologist_timeslot_availability[cur->timeslot-1],dicttime[oldstart-1+i]);
+
+                    cur = cur->next;
+                }
+                return;
+            }
+
+        }
+        else
+            cur = cur->next;
+
+    }
+}
+
+void cancel(node * head, int apptype, char contactn[])
+{
+   node * cur = head;
+   int flag = 0;
+   int startpos;
+
+   while(cur->next!=NULL)
+   {
+      if(!strcmp(cur->contact_number, contactn))
+      {
+         if(flag==0)
+         {
+             startpos = cur->timeslot;
+             flag = 1;
+         }
+         strcpy(cur->name, "TBE");
+         strcpy(cur->contact_number, "TBE");
+         strcpy(cur->type, "TBE");
+         strcpy(cur->reschedule_preference, "TBE");
+      }
+      cur = cur->next;
+   }
+   reschedule(head, apptype, startpos);
+    printf("Your appointment has been successfully cancelled.");
+ }
+
+void cancelinput(node * physician_schedule, node * gensurgeon_schedule, node * radiologist_schedule)
+{
+    char contactn[10];
+    int apptype;
+    printf("\nPlease enter your contact number:\n");
+    scanf("%10s", &contactn);
+    char typeinput[8][30] = {"General Consultancy", "Blood Test/Vaccinations", "ECG Test", "Routine Checkup", "Minor Surgery", "Major Surgery", "X-Ray", "Ultrasound"};
+    printf("\nAppointment type: (Enter serial number 1-8)\n");
+    for(int i=0; i<8; i++)
+    {
+        printf("%d : %s\n",i+1,typeinput[i]);
+    }
+    scanf("%d", &apptype);
+
+    switch(apptype)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            cancel(physician_schedule, apptype, contactn);
+            break;
+        case 5:
+        case 6:
+            cancel(gensurgeon_schedule, apptype, contactn);
+            break;
+        case 7:
+        case 8:
+            cancel(radiologist_schedule, apptype, contactn);
+            break;
+        default:
+            printf("Invalid input.");
+            cancelinput(physician_schedule, gensurgeon_schedule, radiologist_schedule);
+            break;
+    }
+}
+
+
+void enter(node * physician_schedule, node * gensurgeon_schedule, node * radiologist_schedule)
+{
+    int choice;
+    printf("Please enter which doctor's schedule you would like to view:\n1. Physician\n2. General Surgeon\n3. Radiologist\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+            display(physician_schedule);
+            break;
+        case 2:
+            display(gensurgeon_schedule);
+            break;
+        case 3:
+            display(radiologist_schedule);
+            break;
+        default:
+            printf("Cannot find this schedule, please try again.");
+            enter(physician_schedule,gensurgeon_schedule,radiologist_schedule);
+            break;
+    }
+}
+
+
+void admin(node * physician_schedule, node * gensurgeon_schedule, node * radiologist_schedule)
+{
+   char username[]="hospital";
+   char password[]="doctor123";
+   char user[9];
+   char pw[10];
+   int check=0, usercheck, passcheck;
+   printf("Administrator credentials to be entered below.\nUsername:\t");
+   scanf("%s",&user);
+   printf("Password:\t");
+   scanf("%s",&pw);
+   usercheck=strcmp(username,user);
+   passcheck=strcmp(password,pw);
+
+   if(check==usercheck && check==passcheck)
+   {
+       printf("\nWelcome, Admin.\n");
+       enter(physician_schedule, gensurgeon_schedule, radiologist_schedule);
+   }
+   else
+   {
+       printf("\nIncorrect username or password. Please try again.");
+       admin(physician_schedule, gensurgeon_schedule, radiologist_schedule);
+   }
+}
+
+
 void input_details(node * physician_schedule, node * gensurgeon_schedule, node * radiologist_schedule)
 {
-    //Arrays to represent the occupied or vacant status of the timeslots in staff members' schedules (0 represents occupied)
-    char physician_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
-    char gensurgeon_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
-    char radiologist_timeslot_availability[24][8] = {"900","930","1000","1030","1100","1130","1200","1230","1300","1330","1400","1430","1500","1530","1600","1630","1700","1730","1800","1830","1900","1930","2000","2030"};
 
     printf("\nAPPOINTMENT BOOKING SCREEN ----------------------- \n");
     char typeinput[8][30] = {"General Consultancy", "Blood Test/Vaccinations", "ECG Test", "Routine Checkup", "Minor Surgery", "Major Surgery", "X-Ray", "Ultrasound"};
@@ -123,7 +314,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(physician_timeslot_availability[i]!="occupied")
+                if(strcmp(physician_timeslot_availability[i],"occupied")!=0)
                     printf("%d: %s\n", i+1, physician_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -141,7 +332,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(physician_timeslot_availability[i]!="occupied" && physician_timeslot_availability[i+1]!="occupied" && i+1 <24)
+                if(strcmp(physician_timeslot_availability[i],"occupied")!=0 && strcmp(physician_timeslot_availability[i+1],"occupied")!=0 && i+1 <24)
                     printf("%d: %s\n", i+1, physician_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -160,7 +351,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(gensurgeon_timeslot_availability[i] != "occupied" && gensurgeon_timeslot_availability[i+1] != "occupied" && i+1 < 24)
+                if(strcmp(gensurgeon_timeslot_availability[i], "occupied")!=0 && strcmp(gensurgeon_timeslot_availability[i+1], "occupied")!=0 && i+1 < 24)
                     printf("%d: %s\n", i+1, gensurgeon_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -179,7 +370,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(gensurgeon_timeslot_availability[i] != "occupied" && gensurgeon_timeslot_availability[i+1] != "occupied" && i+1 < 24 && gensurgeon_timeslot_availability[i+2] != "occupied" && i+2 < 24 && gensurgeon_timeslot_availability[i+3] != "occupied" && i+3 < 24 && gensurgeon_timeslot_availability[i+4] != "occupied" && i+4 < 24 && gensurgeon_timeslot_availability[i+5] != "occupied" && i+5 < 24)
+                if(strcmp(gensurgeon_timeslot_availability[i], "occupied")!=0 && strcmp(gensurgeon_timeslot_availability[i+1], "occupied")!=0 && i+1 < 24 && strcmp(gensurgeon_timeslot_availability[i+2], "occupied")!=0 && i+2 < 24 && strcmp(gensurgeon_timeslot_availability[i+3], "occupied")!=0 && i+3 < 24 && strcmp(gensurgeon_timeslot_availability[i+4], "occupied")!=0 && i+4 < 24 && strcmp(gensurgeon_timeslot_availability[i+5], "occupied")!=0 && i+5 < 24)
                     printf("%d: %s\n", i+1, gensurgeon_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -198,7 +389,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(radiologist_timeslot_availability[i]!="occupied")
+                if(strcmp(radiologist_timeslot_availability[i], "occupied")!=0)
                     printf("%d: %s\n", i+1, radiologist_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -215,7 +406,7 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
             printf("\nThe currently available timeslots are as follow: \n");
             for(int i=0; i<24; i++)
             {
-                if(radiologist_timeslot_availability[i]!="occupied" && radiologist_timeslot_availability[i+1]!="occupied" && i+1 < 24)
+                if(strcmp(radiologist_timeslot_availability[i], "occupied")!=0 && strcmp(radiologist_timeslot_availability[i+1], "occupied")!=0 && i+1 < 24)
                     printf("%d: %s\n", i+1, radiologist_timeslot_availability[i]);
             }
             printf("Please enter your preferred timeslot : ");
@@ -233,11 +424,11 @@ void input_details(node * physician_schedule, node * gensurgeon_schedule, node *
 
 }
 
-
-
 int main()
 {
+    __label__ mainmenu;
     int n = 24;
+    int menuchoice;
     node * gensurgeon_schedule = NULL;
     node * physician_schedule = NULL;
     node * radiologist_schedule = NULL;
@@ -246,7 +437,27 @@ int main()
     gensurgeon_schedule = createLinkedList(n);
     radiologist_schedule = createLinkedList(n);
 
-    input_details(physician_schedule, gensurgeon_schedule, radiologist_schedule);
+    printf("WELCOME TO THE AUTOMATED HOSPITAL SCHEDULE MANAGEMENT SYSTEM\n\n");
+    
+/*
+    mainmenu:
+        printf("\n|| Main Menu ||\n\n");
+        printf("\n\t1. Admin login.\n\t2. Book an appointment.\n\t3. View your appointment.\n\t4. Cancel an appointment.\n\t5. Exit");
+        printf("\n(Enter index number to select operation)");
+        scanf("%d",&menuchoice);
+        switch(menuchoice)
+        {
+            //case 1:
 
+            case 2:
+                input_details(physician_schedule, gensurgeon_schedule, radiologist_schedule);
+                //case 3:
+                break;
+
+            //case 4:
+
+        }
+        if(menuchoice!=5)
+            goto mainmenu;
+*/
 }
-
